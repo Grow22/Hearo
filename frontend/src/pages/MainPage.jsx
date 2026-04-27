e3de22243762d0f1ff053eba2164fc4924b6c5b0
zezeword <hanhui1823@gmail.com>
2026-04-28 08:19:06 +0900

    초기 환경 세팅

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export default function MainPage() {
+  return (
+    <Container>
+      <h2>header</h2>
+      <div>알람배너</div>
+      <div>기기연결</div>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
